b8471f0c792b24ab2dc58df8535291e24c90771d
sajibsd013 <sajibsd013@gmail.com>
2023-11-13 17:10:36 +0600

    update setting

--- draggable-ui-backend/settings.py
+++ draggable-ui-backend/settings.py
@@ -47,8 +47,8 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(days=1),
-    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=10),
+    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=7),
 }
 # Application definition
 
