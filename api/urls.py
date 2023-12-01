3fefede5e494947022e10e89a0ac77a01839da7d
sajibsd013 <sajibsd013@gmail.com>
2023-12-02 00:17:21 +0600

    CRUD

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import cv
+from .views import cv, cv_detail
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('cv/', cv),
+    path('cv/<int:pk>/', cv_detail),
 ]
--- api/views.py
+++ api/views.py
@@ -26,3 +26,28 @@ def cv(request):
             return Response(cvserializer.data)
         except:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST', 'GET', 'PUT','DELETE'])
+@permission_classes((IsAuthenticated,))
+@authentication_classes((JWTAuthentication,))
+def cv_detail(request, pk):
+    try:
+        data = CV.objects.get(pk=pk)
+    except data.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = CVSerializer(data)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = CVSerializer(
+            data, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        data.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
